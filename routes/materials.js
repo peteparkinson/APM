3ef0cc153ed5b87ad184524c79c37aef147087f5
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2020-04-15 13:06:52 -0400

    updated title

--- routes/materials.js
+++ routes/materials.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('materials', { 
-      title: 'Materials',
+      title: 'Manage Materials',
       message: 'this is where you\'ll manage materials inventory'
     });
 });
