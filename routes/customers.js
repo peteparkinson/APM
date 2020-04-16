64be0cdf7ae2850da05fe412fac731822f8222be
Parkinson <peter.parkinson@techdata.com>
2020-04-16 08:31:57 -0400

    added html elements for edit projects and customers pages

--- routes/customers.js
+++ routes/customers.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('customers', { 
-      title: 'Dustomers',
+      title: 'Customers',
       message: 'this is where you can manage customer information'
     });
 });
