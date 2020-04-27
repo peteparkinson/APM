69c8a3b755426c5367e902dba9133b5560dd4cf0
peteparkinson <pete.parkinson1098@gmail.com>
2020-04-27 18:30:09 -0400

    cleaned up style. updated materials.js

--- routes/materials.js
+++ routes/materials.js
@@ -1,11 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var fileControl = require('../modules/fileControl.js');
 
-/* GET home page. */
+router.use(express.urlencoded());
+
+// GET page ./materials
 router.get('/', function(req, res, next) {
   res.render('materials', { 
       title: 'Manage Materials',
-      message: 'this is where you\'ll manage materials inventory'
+    });
+});
+
+/***********************************
+ * This section submits a new material, kept in 'req.body'.  
+ * the 'addToJSON' method tries to write the passed object to the passed path
+ ***********************************/
+router.post('/submitted', function(req, res) {
+    var file = 'records/materials.json';
+    var newMaterial = JSON.stringify(req.body);
+
+    //fileControl.method.addToFile(newMaterial, file);
+
+    var json = JSON.parse(obj);
+    console.log(json.materials[0]);
+    json.materials.push(obj);    
+    fs.writeFile(file, JSON.stringify(json), function(err){
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
+
+    res.render('submitted', { 
+        title: 'Art Projects Manager',
     });
 });
 
