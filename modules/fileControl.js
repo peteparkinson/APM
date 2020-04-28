945367312ea9956181789f64cbc2f05a68b54924
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2020-04-28 18:24:36 -0400

    cleaned up

--- modules/fileControl.js
+++ modules/fileControl.js
@@ -8,25 +8,16 @@ var methods = {
         try {
             const parsed = JSON.parse(fileContents);
             parsed.materials.push(obj);
-<<<<<<< HEAD
             fs.writeFile(file, JSON.stringify(parsed), function (err) {
-=======
-            fs.writeFile(file, JSON.stringify(parsed), function(err){
->>>>>>> 94c0a410a0896e384328f405ad8eb3ecb4e72ca5
                 if (err) throw err;
                 console.log('The "data to append" was appended to file!');
             });
         } catch (err) {
             console.log(err);
         }
-<<<<<<< HEAD
-=======
     },
 
-    testMe() {
-        console.log('I\'m working');
->>>>>>> 94c0a410a0896e384328f405ad8eb3ecb4e72ca5
-    }
+
 }
 
 exports.method = methods;
