099410823c3232980572914aefd77878ef8cdcf2
peteparkinson <43157092+peteparkinson@users.noreply.github.com>
2020-05-03 10:35:01 -0400

    working version of materials page done

--- modules/fileControl.js
+++ modules/fileControl.js
@@ -2,32 +2,65 @@ const express = require('express');
 const fs = require('fs');
 
 var methods = {
-    //add an object to a JSON array on file
+    //add an object to a given JSON array file
     addToFile: function (obj, file) {
-        const fileContents = fs.readFileSync(file, 'utf8');
+        var fileContents = fs.readFileSync(file, 'utf8');
+        var parsed = JSON.parse(fileContents);
+        if(methods.present(obj, parsed)){
+            methods.removeFromFile(obj.name, file);
+            //refresh parsed data with object removed from array
+            parsed = JSON.parse(fs.readFileSync(file, 'utf8'));
+        }
+        parsed.objects.push(obj);
         try {
-            const parsed = JSON.parse(fileContents);
-            parsed.materials.push(obj);
             fs.writeFile(file, JSON.stringify(parsed), function (err) {
                 if (err) throw err;
-                console.log('The "data to append" was appended to file!');
-            });
+                console.log('Added \"' + obj.name + '\" to ' + file);
+            }); 
         } catch (err) {
             console.log(err);
         }
     },
 
-    removeFromFile: function (obj, file){
-        console.log('connected');
-        const fileContents = fs.readFileSync(file, 'utf8');
-        try {
-            var json = JSON.parse(fileContents);
-            var materials = json.materials;
-            json.materials = materials.filter((item) => { return item.name !== obj.name });
-            fs.writeFileSync(file, JSON.stringify(json, null, 2));
-        } catch (err) {
-            console.log(err);
+    //remove an object from a give JSON array file
+    //reads file into RAM, splices array, overwrites file with spliced array
+    removeFromFile: function (str, file){
+        var fileContents = fs.readFileSync(file, 'utf8'); //parse JSON file into usable array
+        var parsed = JSON.parse(fileContents);
+        for(let i = 0; i < parsed.objects.length; i++){
+            if(parsed.objects[i].name == str){     
+                parsed.objects.splice(i, 1);                //removes item from array
+                try {
+                    fs.writeFileSync(file, JSON.stringify(parsed));
+                    console.log('Deleted \"' + str + '\" from ' + file);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
         }
+    },
+    
+    getSerial: function(file){
+        const fileContents = fs.readFileSync(file, 'utf8');
+        var parsed = JSON.parse(fileContents);
+        var ser = 1000;
+        for(let i = 0; i < parsed.objects.length ; i++){
+            if(parsed.objects[i].serial >= ser){
+                ser = parsed.objects[i].serial + 1;
+            }
+        }
+        console.log("Assigned serial numer: " + ser)
+        return ser;
+    },
+
+    //checks for presence of object in an array
+    present: function(obj, arr){
+        for(let i = 0; i < arr.objects.length; i++){
+            if(arr.objects[i].name == obj.name){     
+                return true;
+            }
+        }
+        return false;
     },
 
     demo: function(){
