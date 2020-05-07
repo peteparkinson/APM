a376e9a724b90f06731aec4c199406ec58c9e09d
Pete Parkinson <pete.parkinson1098@gmail.com>
2020-05-07 13:39:01 -0400

    started customers page. fixed bug on edit proj. page

--- routes/customers.js
+++ routes/customers.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+const customersPath = './records/customers.json';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var allCustomers = JSON.parse(fs.readFileSync(customersPath, 'utf8'));
     res.render('customers', { 
         title: 'Customers',
-        message: 'this is where you can manage customer information'
+        allCustomers,
+
     });
+});
+
+//on post from edit projects page
+router.post('/submitted', function (req, res) {
+    console.log(req.body.relMaterials);
+
+    res.redirect('/customers');
+});
+
+
+//delete project post response
+router.post('/deleted', function (req, res) {
+    res.redirect('/customers');
 });
 
 module.exports = router;
--- routes/editprojects.js
+++ routes/editprojects.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 
 const fileControl = require('../modules/fileControl.js');
 const listControl = require('../modules/listControl.js');
+const customersPath = './records/customers.json';
 const projectsPath = './records/projects.json';
 const materialsPath = './records/materials.json';
 
@@ -15,12 +16,14 @@ router.use(bodyParser.urlencoded({extended: true}));
 router.get('/', function (req, res, next) {
     var allProjects = JSON.parse(fs.readFileSync(projectsPath, 'utf8'));
     var allMaterials = JSON.parse(fs.readFileSync(materialsPath, 'utf8'));
+    var allCustomers = JSON.parse(fs.readFileSync(customersPath, 'utf8'));
     var proStr = JSON.stringify(allProjects);
     var types = listControl.projectTypes;
     res.render('editprojects', {
         title: 'New / Edit Projects',
         allProjects,
         allMaterials,
+        allCustomers,
         proStr,
         types,
 
@@ -29,9 +32,6 @@ router.get('/', function (req, res, next) {
 
 //on post from edit projects page
 router.post('/submitted', function (req, res) {
-    console.log(req.body.relMaterials);
-
-
     req.body.serial = fileControl.method.getSerial(projectsPath);
     req.body.status = "open";
     req.body.hours = "0";
