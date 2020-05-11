bdfd3c702f8d72ed73bcb650f516bbc3f9994dfa
Pete Parkinson <pete.parkinson1098@gmail.com>
2020-05-11 12:42:57 -0400

    started adding functionality to customers page

--- routes/customers.js
+++ routes/customers.js
@@ -1,29 +1,36 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var bodyParser = require('body-parser');
 
+const fileControl = require('../modules/fileControl.js');
 const customersPath = './records/customers.json';
+
+router.use(express.urlencoded({ extended: true }));
+router.use(bodyParser.urlencoded({ extended: true }));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var allCustomers = JSON.parse(fs.readFileSync(customersPath, 'utf8'));
+    var custStr = JSON.stringify(allCustomers);
     res.render('customers', { 
         title: 'Customers',
         allCustomers,
-
+        custStr,
     });
 });
 
 //on post from edit projects page
 router.post('/submitted', function (req, res) {
-    console.log(req.body.relMaterials);
-
+    req.body.serial = fileControl.method.getSerial(customersPath);
+    fileControl.method.addToFile(req.body, customersPath);
     res.redirect('/customers');
 });
 
 
 //delete project post response
 router.post('/deleted', function (req, res) {
+
     res.redirect('/customers');
 });
 
