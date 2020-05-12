9ddb5201d0c5be7c1695dac91bde5fbab2de080a
Pete Parkinson <pete.parkinson1098@gmail.com>
2020-05-12 11:56:08 -0400

    finished materials, edit projects, and customers pages

--- routes/customers.js
+++ routes/customers.js
@@ -22,6 +22,16 @@ router.get('/', function(req, res, next) {
 
 //on post from edit projects page
 router.post('/submitted', function (req, res) {
+    
+    if (req.body.relProjects) {
+        //interpret relProjects as an array if there's only 1 elemet
+        if (!Array.isArray(req.body.relProjects)) {
+            req.body.relProjects = [req.body.relProjects];
+        }
+    }
+
+    console.log(req.body.name);
+    console.log(req.body.relProjects);
     req.body.serial = fileControl.method.getSerial(customersPath);
     fileControl.method.addToFile(req.body, customersPath);
     res.redirect('/customers');
