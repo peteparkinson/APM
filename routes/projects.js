17dfced619c56d690a905494d99d550713ff030d
Pete Parkinson <pete.parkinson1098@gmail.com>
2020-05-13 06:03:10 -0400

    view projects page, added calculate meterials cost function

--- routes/projects.js
+++ routes/projects.js
@@ -2,15 +2,19 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+const materialsPath = './records/materials.json';
 const projectsPath = './records/projects.json';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    var allMaterials = JSON.parse(fs.readFileSync(materialsPath, 'utf8'));
     var allProjects = JSON.parse(fs.readFileSync(projectsPath, 'utf8'));
+    var matStr = JSON.stringify(allMaterials);
     var proStr = JSON.stringify(allProjects);
     res.render('projects', {
         title: 'Projects',
         allProjects,
+        matStr,
         proStr,
     });
 });
